8d12deb4ba44118c6695c7734c768602bfa04789
StyMaar <inscription@stymaar.fr>
2016-08-29 04:30:58 +0200

    work on the admin page's style

--- admin/index.php
+++ admin/index.php
@@ -3,9 +3,7 @@ include("../head.php");
 ?>
 
 <form method="post" action="upload.php" enctype="multipart/form-data">
-	<textarea name="message" rows="8" cols="45">
-		Votre message ici.
-	</textarea>
+	<textarea name="message" rows="8"></textarea>
 	<input type="file" name="monfichier" /><br />
 	<input type="submit" value="Envoyer le fichier" />
 </form>
--- head.php
+++ head.php
@@ -5,7 +5,7 @@
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<meta name="format-detection" content="telephone=no" />
 		<meta name="viewport" content="width=device-width,initial-scale=1.0" />
-    	<link type="text/css" rel="stylesheet" href="style.css" />
+    	<link type="text/css" rel="stylesheet" href="/microblog/style.css" />
 	</head>
 	<body>
 		<h1>Deux roues pour demain</h1>
