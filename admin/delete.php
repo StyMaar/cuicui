9f4ef8ce6ef573a24d39c950ee0e94783107511d
StyMaar <inscription@stymaar.fr>
2016-09-14 18:25:47 +0200

    add a feature to delete posts

---
+++ admin/delete.php
@@ -0,0 +1,20 @@
+<?php 
+$id = $_GET['id'];
+
+$string = file_get_contents("../content/messages.json");
+$messages = json_decode($string, true);
+
+if($messages[$id]) {
+	unlink('../content/messages.json.bckp');
+	copy('../content/messages.json', '../content/messages.json.bckp'); //create a backup of the storage file
+
+	unset($messages[$id]);
+
+	$fh = fopen('../content/messages.json', 'w');
+	fwrite($fh, json_encode($messages));
+	fclose($fh);
+	header("Location: /microblog/", true, 307);
+} else {
+	echo "failed to remove an item that doesn't exist. id = $id";
+}
+?>
