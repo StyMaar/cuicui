3a484ae95673bfbe59cc2e5ce200af01238c6e42
StyMaar <inscription@stymaar.fr>
2024-01-15 20:30:54 +0100

    Fix single post

--- article.php
+++ article.php
@@ -10,9 +10,33 @@ $message = $messages[$id];
 if($message) {
 	echo "<article>";
 	if (array_key_exists ("images", $message)){
-		foreach ($message['images'] as $url) {
-			echo "<img src='$url' alt='Image manquante' />";
+		foreach ($message['images'] as $image) {
+			$url = $image["url"];
+			$name = $image["name"];
+			echo "<img src='$url' alt='Image manquante' title='$name'/>";
 		}
+	}
+	if (array_key_exists ("videos", $message)){
+		foreach ($message['videos'] as $video) {
+			$url = $video["url"];
+			echo "<video controls>";
+			echo "<source src='$url' type='video/mp4' />";
+			echo "<p>";
+			echo "	Votre navigateur ne prend pas en charge les vidéos HTML5. Voici";
+			echo "	<a href='$url'>un lien pour télécharger la vidéo</a>.";
+			echo "</p>";
+			echo "</video>";
+		}
+	}
+	if (array_key_exists ("misc", $message)){
+		echo "<h3>Fichiers</h3>";
+		echo "<ul>";
+		foreach ($message['misc'] as $fichier) {
+			$url = $fichier["url"];
+			$name = $fichier["name"];
+			echo "<li><a href='$url'>$name</a></li>";
+		}
+		echo "</ul>";
 	}
 	echo "<p>";
 	echo $message['content'];
@@ -22,10 +46,11 @@ if($message) {
 	echo date(DATE_RFC2822, $message['date']);
 	echo "</p>";
 	echo "</article>";
-	
 } else {
 	echo "that item doesn't exist. id = $id";
 }
 
 include("footer.php");
 ?>
+
+	
