b5d5e10181e03eeb4d302f2ba26fd741b8151bec
StyMaar <inscription@stymaar.fr>
2024-02-03 23:18:10 +0100

    add favicon

--- head.php
+++ head.php
@@ -9,6 +9,7 @@ include("check-auth.php");
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<meta name="format-detection" content="telephone=no" />
 		<meta name="viewport" content="width=device-width,initial-scale=1.0" />
+		<link rel="icon" href="favicon.ico" />
     	<link type="text/css" rel="stylesheet" href="style.css" />
 	</head>
 
