0ddcabe99270b3d1ef8a438dc2e78766a46b0212
StyMaar <inscription@stymaar.fr>
2024-02-27 15:47:00 +0100

    add photos to the logins

--- auth.php
+++ auth.php
@@ -4,7 +4,8 @@
         'papy' => '$2y$10$bph5h3RpsMKSl7oeY7Dn6erhko0auaRl7oqmaB60x102gc7ehTUAC',
         'amatxi' => '$2y$10$rcRZxNdZWB8IXNKlRMoH0u/WC4qaPEI8NxUQ0izcPXJSpSGeTeQfO',
         'papanou' => '$2y$10$1y8IXWVZgoi0yBr2DOKJ7eIo2noUSObBGmCvzBUu2JOdKoi7ioo66',
-        'marie-pia' => '$2y$10$seX4B0sXzE4t0XoN/It5DePgvVGAznMARpfTEnij3yAC8e1NwQ4y.'
+        'marie-pia' => '$2y$10$seX4B0sXzE4t0XoN/It5DePgvVGAznMARpfTEnij3yAC8e1NwQ4y.',
+        'photos' => '$2y$10$6S.mKU7P8n.Tv1P7aCIxFuWq19gl0ti6LvQTj4UaRsppNVxX.o0La'
     ];
 
     session_start();
